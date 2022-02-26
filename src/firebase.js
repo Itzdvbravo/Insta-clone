04fc88bdeeb59075b53e46a13f98d8eba40bada8
OldBravo <47894997+Itzdvbravo@users.noreply.github.com>
2022-02-26 14:19:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase/compat";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDcmYQf9KEAJCoqceU7bCbCRmmnPO7Gdco",
-    authDomain: "my-own-insta.firebaseapp.com",
-    projectId: "my-own-insta",
-    storageBucket: "my-own-insta.appspot.com",
-    messagingSenderId: "241691889033",
-    appId: "1:241691889033:web:4a26994d963d0c8f99a558"
+    /**/
 });
 
 const db = firebaseApp.firestore();
